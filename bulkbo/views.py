9a0305e868ee2d438fcfcf5c4cb940605d190e83
Phansivang <Phansivang@gmail.com>
2022-07-27 18:50:52 +0700

    6:50

--- bulkbo/views.py
+++ bulkbo/views.py
@@ -1,17 +1,20 @@
 import requests
-from django.shortcuts import render,redirect
 from django.contrib import messages
-from .forms import register,LoginForm
 from django.contrib.auth import views as auth_views
-from django.views import generic
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from django.views import generic
+
+from .forms import register, LoginForm
+
 
 @login_required(login_url='login')
 def homepage(request):
     get_balance = requests.get(
-        'https://gateway.sms77.io/api/balance?p=w8f7Cjk6jKEH7NkyAiuNZ4PQn25wXbjGINLqKW7adpeaQpunakzV276X3Zn3jgS9')
-    return render(request,'home.html',{'balance':get_balance.text})
+        'https://gateway.sms77.io/api/balance?p=')
+    return render(request, 'home.html', {'balance': get_balance.text})
+
 
 def registerview(request):
     if request.method == 'POST':
@@ -27,9 +30,11 @@ def registerview(request):
     print(3)
     return render(request, 'register.html', {'form': form})
 
+
 class LoginView(auth_views.LoginView):
     form_class = LoginForm
     template_name = 'login.html'
 
+
 def configview(request):
-    return render(request,'config.html')
+    return render(request, 'config.html')
